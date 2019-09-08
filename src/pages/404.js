d1eaf9764a207100f4d2b8e6343c11882e7ec042
Ben Awad <benawad97@gmail.com>
2019-09-08 16:34:13 -0500

    404 -> home

--- src/pages/404.js
+++ src/pages/404.js
@@ -14,6 +14,7 @@ class NotFoundPage extends React.Component {
         <SEO title="404: Not Found" />
         <h1>Not Found</h1>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <Link to="/">go back home</Link>
       </Layout>
     )
   }
